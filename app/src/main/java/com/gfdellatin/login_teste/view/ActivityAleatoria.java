7cb2689f66384fe7e02f4f719987e19003177aa8
GuilhermeDellatin <guidellatin@gmail.com>
2021-01-05 22:40:16 -0300

    DirecionaActivity por intent

---
+++ app/src/main/java/com/gfdellatin/login_teste/view/ActivityAleatoria.java
@@ -0,0 +1,16 @@
+package com.gfdellatin.login_teste.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gfdellatin.login_teste.R;
+
+public class ActivityAleatoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aleatoria);
+    }
+}
--- app/src/main/java/com/gfdellatin/login_teste/view/MainActivity.java
+++ app/src/main/java/com/gfdellatin/login_teste/view/MainActivity.java
@@ -2,10 +2,10 @@ package com.gfdellatin.login_teste.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.gfdellatin.login_teste.R;
@@ -18,13 +18,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        direcionaTela();
+        /*
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
         Fragment fragment = new LoginFragment();
-        int i = 1;
+        int i = 3;
         if (i == 2) fragment = new ConfigFragment();
+        if (i == 3) {
+            Intent intent = new Intent(getApplicationContext(), ActivityAleatoria.class);
+            startActivity(intent);
+        }
         if(fragment instanceof ConfigFragment){
             System.out.println("config");
         } else {
@@ -32,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         }
         fragmentTransaction.add(R.id.main_fragment, fragment);
         fragmentTransaction.commit();
-
+*/
     }
 /*
     @Override
@@ -48,4 +53,25 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.add(R.id.main_fragment, fragment);
         fragmentTransaction.commit();
     }*/
+
+    public void direcionaTela() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Fragment fragment = new Fragment();
+        int i = 3;
+        if (i == 1) fragment= new LoginFragment();
+        if (i == 2) fragment = new ConfigFragment();
+        if (i == 3) {
+            Intent intent = new Intent(getApplicationContext(), ActivityAleatoria.class);
+            startActivity(intent);
+        }
+        if(fragment instanceof ConfigFragment){
+            System.out.println("config");
+        } else {
+            System.out.println("login");
+        }
+        fragmentTransaction.add(R.id.main_fragment, fragment);
+        fragmentTransaction.commit();
+    }
 }
